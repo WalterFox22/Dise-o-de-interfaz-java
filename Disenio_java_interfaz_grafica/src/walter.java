8a97791a9926daa4842dbf5b7536f84437de4716
WalterFox <john.cobacango@epn.edu.ec>
2024-06-20 09:37:46 -0500

    jug

--- Disenio_java_interfaz_grafica/src/walter.java
+++ Disenio_java_interfaz_grafica/src/walter.java
@@ -3,4 +3,10 @@ import javax.swing.*;
 public class walter {
     private JButton button1;
     private JPanel panel1;
+    private JCheckBox eresPolitecnicoCheckBox;
+    private JList list1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
